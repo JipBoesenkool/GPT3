ef31e7c6aa48ac96effd53e4f182b2d886b54379
SanderBoelaars <sanderboelaars@hotmail.com>
2016-12-01 15:45:03 +0100

    Commit: Demo

---
+++ GPT/Assets/Scripts/TargetBehaviorScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBehaviorScript : MonoBehaviour {
+
+    public enum Shape { Square, Circle, Triangle, Pentagon};
+
+    public Color color;
+    public Shape shape;
+
+	// Use this for initialization
+	void Start () {      
+        this.GetComponent<SpriteRenderer>().color = color;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ GPT/Assets/Scripts/TargetMovementScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMovementScript : MonoBehaviour {
+
+    public bool inSchool;
+    public GameObject leader;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        inSchool = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (inSchool == true)
+        {
+            this.transform.position = Vector2.MoveTowards((leader.transform.position - new Vector3(4, 0, 0)), this.transform.position, speed * Time.deltaTime);
+        }
+    }
+}
