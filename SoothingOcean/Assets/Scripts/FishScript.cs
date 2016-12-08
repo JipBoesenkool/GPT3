9cdc1ddf517d816d6307ffcac4ac9f4cd219445d
SanderBoelaars <sanderboelaars@hotmail.com>
2016-12-08 15:08:06 +0100

    Commit: Fish and Player Scripts

---
+++ SoothingOcean/Assets/Scripts/FishScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishScript : MonoBehaviour
+{
+    public enum FishType
+    {
+        Trout,
+        Tuna,
+        Bristlemouth,
+        Forelle
+    };
+
+    public Color color;
+    public FishType type;
+    public float size;
+    public bool inSchool;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (size == 0)
+        {
+            size = 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ SoothingOcean/Assets/Scripts/PlayerFishRecruitScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFishRecruitScript : MonoBehaviour
+{
+    public List<GameObject> school;
+    public bool easyMode;
+
+    // Use this for initialization
+    void Start()
+    {
+        school = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Fish"))
+        {
+            if (!other.GetComponent<FishScript>().inSchool)
+            {
+                if (!easyMode)
+                {
+                    Debug.Log("ayyyyy");
+                    if (school.Count <= 0)
+                    {
+                        other.gameObject.transform.SetParent(GameObject.Find("School").transform);
+                        other.GetComponent<Collider>().enabled = false;
+                        school.Add(other.gameObject);
+                    }
+                    else if (school.Count > 0)
+                    {
+                        foreach (GameObject fish in school)
+                        {
+                            //Only add fish if you already have fish of the same color.
+                            if (fish.GetComponent<FishScript>().color.Equals(other.GetComponent<FishScript>().color))
+                            {
+                                other.gameObject.transform.SetParent(GameObject.Find("School").transform);
+                                other.GetComponent<Collider>().enabled = false;
+                                school.Add(other.gameObject);
+                                break;
+                            }
+                            else if (fish.GetComponent<FishScript>().type.Equals(other.GetComponent<FishScript>().type))
+                            {
+                                other.gameObject.transform.SetParent(GameObject.Find("School").transform);
+                                other.GetComponent<Collider>().enabled = false;
+                                school.Add(other.gameObject);
+                                break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    other.gameObject.transform.SetParent(GameObject.Find("School").transform);
+                    school.Add(other.gameObject);
+                    other.GetComponent<Collider>().enabled = false;
+                }
+            }
+        }
+    }
+}
