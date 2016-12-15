8592e59c04311b426f768199607e77033808be8a
Lesley student <lesley.peters@live.nl>
2016-12-15 14:03:59 +0100

    fishspawner fixed

--- SoothingOcean/Assets/Scripts/SpawnPoint.cs
+++ SoothingOcean/Assets/Scripts/SpawnPoint.cs
@@ -3,16 +3,16 @@
 /// <summary>
 /// 1. Maak een empty game object en noem het SpawnManager.
 /// 2. Maak een of meerdere empty game objects, noem ze SpawnPoint en plaats ze onder de SpawnManager.
-/// 3. Voeg alle enemies toe die door dit spawnpunt gespawnd kunnen worden.
+/// 3. Voeg alle moedels toe die door dit spawnpunt gespawnd kunnen worden.
 /// </summary>
 
 public class SpawnPoint : MonoBehaviour
 {
-	public Transform spawnPoint;			// Punt waar enemies gespawned worden.
 	public float spawnTime = 3f;        	// Interval tussen spawns.
 	public bool randomSpawnTime = false;	// False gebruikt een vaste spawn tijd (zoals hierboven aangegeven), true gebruikt een random spawntijd tussen 0 en de aangegeven spawntijd.
 	public float maxScale = 1;				// Maakt de objecten die gespawned worden groter.
-	public GameObject[] enemies;          	// Array van enemies die gespawnd kunnen worden.
+	public GameObject[] models;          	// Array van models die gespawnd kunnen worden.
+	public Transform[] spawnPoints;        // Array van spawnpunten (Empty game objects) waar de models gespawnd worden.
 
 	void Start ()
 	{
@@ -38,9 +38,10 @@ public class SpawnPoint : MonoBehaviour
 	void Spawn ()
 	{
 		// random nummer voor het kiezen welk spawnpunt gebruikt word.
-		int enemyIndex = Random.Range (0, enemies.Length);
-		// Maakt een instantie van de enemy prefab en plaatst het op een random spawnpoint.
-		GameObject spawnedobject = Instantiate (enemies[enemyIndex], spawnPoint.position, spawnPoint.rotation);
+		int modelIndex = Random.Range (0, models.Length);
+		int spawnPointIndex = Random.Range (0,spawnPoints.Length);
+		// Maakt een instantie van de model prefab en plaatst het op een random spawnpoint.
+		GameObject spawnedobject = Instantiate (models[modelIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
 		spawnedobject.transform.localScale = Vector3.one * Random.Range(1, maxScale);
 	}
 }
