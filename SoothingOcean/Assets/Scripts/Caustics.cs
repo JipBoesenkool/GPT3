13db09ee7b848cd1df3f15ee287071b8c750f2c0
SebastiaanvGaans <sebastiaanvgaans@hotmail.com>
2016-12-16 09:57:06 +0100

    Caustics fix?
    Lets hope

--- SoothingOcean/Assets/Scripts/Caustics.cs
+++ SoothingOcean/Assets/Scripts/Caustics.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class Caustics : MonoBehaviour {
 
+#if UNITY_ANDROID
+#elif UNITY_IOS
+#else
     private Projector projector;
     public MovieTexture mt;
 
+#endif
     void Start()
     {
+#if UNITY_ANDROID
+#elif UNITY_IOS
+#else
         projector = GetComponent<Projector>();
         projector.material.SetTexture("_ShadowTex", mt);
         mt.loop = true;
         mt.Play();
+#endif
     }
 }
