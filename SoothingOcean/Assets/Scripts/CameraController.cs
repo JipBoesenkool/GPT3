2533f142e547a6a011ef82a6c65ef331a46ef6a9
SebastiaanvGaans <sebastiaanvgaans@hotmail.com>
2017-01-13 10:27:23 +0100

    Smooth zoom on de/increase of school

--- SoothingOcean/Assets/Scripts/CameraController.cs
+++ SoothingOcean/Assets/Scripts/CameraController.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
     public Transform target;
     public float rotationSmoothing;
-    public int flocksize = 1;
-    public int newFlock = 1;
-    public int initialDistance = 20;
+    public float zoomSmoothing;
+    public float zoomPerFish;
+
+    public int initialDistance;
+    Vector3 distanceVector;
+
+    public BoidController bc;
 
     private float currentX;
-    private float currentY;
+    private float currentY; 
 
-
-    Vector3 distance;
-    Vector3 increment;
 	// Use this for initialization
 	void Start () {
-        distance = new Vector3(0, 0, initialDistance);
         //Debug.Log(distance);
     }
 	
@@ -25,19 +25,13 @@ public class CameraController : MonoBehaviour {
 	void LateUpdate () {
         currentY = RotateAroundY();
         currentX = RotateAroundX();
-
         Quaternion rotation = Quaternion.Euler(target.eulerAngles.x, currentY, 0);
 
-        if (flocksize !=  newFlock)
-        {
-            distance = distance + new Vector3(0, 0, 3);
-            flocksize = newFlock;
-        }
-
-        //Debug.Log(rotation);
+        //get current view distance
+        distanceVector = new Vector3(0, 0, CalculateViewDistance());
 
         //from angle, find a position at distance BEHIND(that why -) target, look at target
-        transform.position = target.position + (rotation * -distance);
+        transform.position = target.position + (rotation * -distanceVector);
         transform.LookAt(target.transform);
     }
 
@@ -59,5 +53,21 @@ public class CameraController : MonoBehaviour {
         float angle = Mathf.Lerp(currentX, wantedX, rotationSmoothing * Time.deltaTime);
         //Debug.Log(angle);
         return angle;
+    }
+
+    float CalculateViewDistance()
+    {
+        //Calculate distance wanted
+        float distance;
+        distance = initialDistance + zoomPerFish * bc.school.Count;
+
+        //Debug.Log(distance);
+
+        //Damped the transition if distance is changed
+        distance =  Mathf.Lerp(distanceVector.z, distance, zoomSmoothing * Time.deltaTime);
+
+        Debug.Log(distance);
+
+        return distance;
     }
 }
