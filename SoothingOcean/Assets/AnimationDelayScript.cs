e198fb237cfde9944adc0c2dad4e6c0b89eb2605
SanderBoelaars <sanderboelaars@hotmail.com>
2017-01-19 15:51:44 +0100

    Commit: Met nieuwe vissen G!

---
+++ SoothingOcean/Assets/AnimationDelayScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationDelayScript : MonoBehaviour {
+
+    public Animation anim;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(waiter());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    IEnumerator waiter()
+    {
+        while (true)
+        {
+            int wait_time = Random.Range(0, 4);
+            anim.Play("Basic");
+            yield return new WaitForSeconds(wait_time);
+        }
+    }
+}
